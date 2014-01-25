ce3b0a5084839561aac4e11101546b618c611300
Igor Ramadas <igor@devv.com>
2014-01-25 19:32:31 +0100

    Updates, updates, updates.

---
+++ phone/Entities/Weather.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AylaPhone.Entities
+{
+    public class Weather
+    {
+        public Double Temperature { get; set; }
+        public Double Humidity { get; set; }
+        public Int32 CO2 { get; set; }
+    }
+}
---
+++ phone/Lib/HomeService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using AylaPhone.Entities;
+
+namespace AylaPhone
+{
+    public class HomeService
+    {
+        #region Home services
+
+        public static List<Light> GetLights()
+        {
+            
+        }
+
+        #endregion
+    }
+}
