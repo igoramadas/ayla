8dcc89a5692290954e14be09a29bfce28f2e2603
Igor Ramadas <igor@devv.com>
2014-04-19 01:05:48 +0200

    Creating new universal Windows Store app.

---
+++ app/Shared/PhonePage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Windows.ApplicationModel.Resources;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace Ayla.App
+{
+    public class PhonePage : Page
+	{
+        #region Properties
+
+        protected Boolean BackCancel { get; set; }
+
+        #endregion
+
+		#region Base methods
+
+        public PhonePage()
+		{
+		}
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var appResources = new ResourceLoader();
+
+            base.OnNavigatedTo(e);
+
+            var topTitle = FindName("TopTitle");
+
+            if (topTitle != null)
+            {
+                ((TextBlock)topTitle).Style = (Style)Application.Current.Resources["TopTitle"];
+                ((TextBlock) topTitle).Text = appResources.GetString("AppTitle");
+            }
+        }
+
+		#endregion
+	}
+}
---
+++ app/WindowsPhone/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using Ayla.App.Common;
+
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace Ayla.App
+{
+    public sealed partial class MainPage
+    {
+        private NavigationHelper navigationHelper;
+        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            navigationHelper = new NavigationHelper(this);
+            navigationHelper.LoadState += NavigationHelper_LoadState;
+            navigationHelper.SaveState += NavigationHelper_SaveState;
+        }
+
+        public NavigationHelper NavigationHelper
+        {
+            get { return navigationHelper; }
+        }
+
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return defaultViewModel; }
+        }
+
+        private void NavigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+        }
+
+        private void NavigationHelper_SaveState(object sender, SaveStateEventArgs e)
+        {
+        }
+
+        #region NavigationHelper registration
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+    }
+}
